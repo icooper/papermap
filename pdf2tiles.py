f09b78fab8ecaec6d9a418dd6f4d29452433c55a
Ian Cooper <me@iancooper.name>
2020-08-15 16:02:10 -0600

    added MBTiles file output

--- pdf2tiles.py
+++ pdf2tiles.py
@@ -8,9 +8,12 @@ import json
 import math
 import os.path
 import pprint
-import re
+import re 
+import shutil
+import sqlite3
 import subprocess
 import sys
+import xml.etree.ElementTree as ET
 
 # these might have to be updated
 GDALINFO = '/usr/bin/gdalinfo'
@@ -193,6 +196,133 @@ def tile(analysis: Dict, infile: str, outdir: str, min_zoom=0, max_zoom=0):
     passthru(args, prefix=True)
     return outdir
 
+# pack a tileset directory into a MBTiles file
+def mbtiles_pack(indir: str, outfile: str, *, debug=False):
+    
+    # metadata fields
+    metadata = {
+        # required fields
+        'name': None,
+        'format': 'png',
+
+        # recommended fields
+        'bounds': None,
+        'center': None,
+        'minzoom': None,
+        'maxzoom': None
+
+        # optional fields
+        #'attribution': None,
+        #'description': None,
+        #'type': None, # overlay or baselayer
+        #'version': None
+    }
+
+    # check for a tilemapresource.xml file
+    tmrfilename = os.path.join(indir, 'tilemapresource.xml')
+    if not os.path.isfile(tmrfilename):
+        print('Error: Tileset ', tileset, 'missing tilemapresource.xml file.')
+        return
+
+    # parse tilemapresource.xml
+    tilemap = ET.parse(tmrfilename).getroot()
+    metadata['name'] = tilemap.find('./Title').text
+    
+    # get zoom levels
+    zoomlevels = list(map(lambda x: int(x.attrib['href']), tilemap.findall('./TileSets/TileSet')))
+    metadata['minzoom'] = min(zoomlevels)
+    metadata['maxzoom'] = max(zoomlevels)
+
+    # get bounds and center
+    tilemapbb = tilemap.find('./BoundingBox')
+    minx = tilemapbb.attrib['minx']
+    miny = tilemapbb.attrib['miny']
+    maxx = tilemapbb.attrib['maxx']
+    maxy = tilemapbb.attrib['maxy']
+    centerx = str((float(maxx) - float(minx)) / 2.0 + float(minx))
+    centery = str((float(maxy) - float(miny)) / 2.0 + float(miny))
+    metadata['bounds'] = ','.join([minx, miny, maxx, maxy])
+    metadata['center'] = ','.join([centerx, centery, str(int((metadata['maxzoom'] - metadata['minzoom']) / 2 + metadata['minzoom']))])
+    if debug:
+        print('Metadata from ', tmrfilename, ':', sep='')
+        print(pprint.pformat(metadata, indent=4))
+
+    # create the sqlite3 database and set up the connection
+    db = sqlite3.connect(outfile)
+    cursor = db.cursor()
+    cursor.execute('PRAGMA synchronus=0;')
+    cursor.execute('PRAGMA locking_mode=EXCLUSIVE;')
+    cursor.execute('PRAGMA journal_mode=DELETE;')
+    if debug:
+        print('Created MBTiles file', outfile, 'and set up SQLite3 connection')
+
+    # create some tables
+    cursor.execute('CREATE TABLE tiles (zoom_level INTEGER, tile_column INTEGER, tile_row INTEGER, tile_data BLOB);')
+    cursor.execute('CREATE TABLE metadata (name TEXT, value TEXT);')
+    if debug:
+        print('Created main tables')
+
+    # create some indexes
+    cursor.execute('CREATE UNIQUE INDEX name ON metadata (name);')
+    cursor.execute('CREATE UNIQUE INDEX tile_index ON tiles (zoom_level, tile_column, tile_row);')
+    if debug:
+        print('Created indexes for main tables')
+
+    # create more tables that are required but we won't use
+    cursor.execute('CREATE TABLE grids (zoom_level INTEGER, tile_column INTEGER, tile_row INTEGER, grid BLOB);')
+    cursor.execute('CREATE TABLE grid_data (zoom_level INTEGER, tile_column INTEGER, tile_row INTEGER, key_name TEXT, key_json TEXT);')
+    if debug:
+        print('Created other required tables')
+
+    # write metadata
+    for name, value in metadata.items():
+        if value:
+            cursor.execute('INSERT INTO metadata (name, value) VALUES (?, ?)', (name, value))
+            if debug:
+                print('Inserted metadata "%s" = "%s"' % (name, value))
+
+    # get Z directories based on what we found in tilemapresource.xml
+    for z in [str(z) for z in zoomlevels if os.path.isdir(os.path.join(indir, str(z)))]:
+        if debug:
+            print('Z =', z, '- Found directory:', os.path.join(indir, z))
+
+        # get X directories for this Z
+        for x in [x for x in os.listdir(os.path.join(indir, z)) if os.path.isdir(os.path.join(indir, z, x))]:
+            if debug:
+                print('Z =', z, ', X =', x, '- Found directory:', os.path.join(indir, z, x))
+
+            # get Y files for this Z, X
+            for ypng in [y for y in os.listdir(os.path.join(indir, z, x)) if os.path.isfile(os.path.join(indir, z, x, y)) and y.lower().endswith('.%s' % metadata['format'])]:
+                y = os.path.splitext(ypng)[0]
+                if debug:
+                    print('Z =', z, ', X =', x, ', Y =', y, '- Found Y file:', os.path.join(indir, z, x, ypng))
+
+                # read the binary image data
+                fh = open(os.path.join(indir, z, x, ypng), 'rb')
+                ypng_data = fh.read()
+                fh.close()
+
+                # insert the image data into the database
+                cursor.execute(
+                    'INSERT INTO tiles (zoom_level, tile_column, tile_row, tile_data) VALUES (?, ?, ?, ?);',
+                    (z, x, y, sqlite3.Binary(ypng_data)))
+                if debug:
+                    print('Z =', z, ', X =', x, ', Y =', y, '- Inserted image data into database')
+
+    # clean up database
+    db.commit()
+    cursor.execute('ANALYZE;')
+    cursor.execute('VACUUM;')
+    if debug:
+        print('Analyzed and optimized SQLite3 database')
+    
+    # done!
+    cursor.close()
+    db.close()
+    if debug:
+        print('Closed SQLite3 database connection to MBTiles file')
+    print('Packed tileset into MBTiles file', outfile)
+
 @click.group()
 def cli():
     '''Utility to convert GeoPDF to GeoTIFF or tileset using GDAL'''
@@ -254,7 +384,7 @@ def tiff(dpi: int, max_zoom: int, infile: str, outfile: str):
 @click.argument('infile')
 @click.argument('outdir', default='__auto__')
 def tileset(dpi: int, min_zoom: int, max_zoom: int, infile: str, outdir: str):
-    '''Converts GeoPDF into tileset folder'''
+    '''Converts GeoPDF into tileset directory'''
     infile_split = os.path.splitext(infile)
 
     # get output path if not specified
@@ -265,6 +395,10 @@ def tileset(dpi: int, min_zoom: int, max_zoom: int, infile: str, outdir: str):
     if os.path.isfile(outdir):
         print('Error: Output directory', outdir, 'is a file.')
 
+    # make sure the output path doesn't exist
+    elif os.path.isdir(outdir):
+        print('Error: Output directory', outdir, 'already exists.')
+
     # rasterize and generate tiles
     else:
 
@@ -273,19 +407,50 @@ def tileset(dpi: int, min_zoom: int, max_zoom: int, infile: str, outdir: str):
 
         # rasterize the PDF file
         if analysis['format'] == 'PDF/Geospatial PDF':
-            infile = rasterize(analysis, infile, '%s.tiff' % infile_split[0])
+            temptiff = rasterize(analysis, infile, '%s_temp.tiff' % outdir)
 
             # tile the TIFF file
-            tile(analysis, infile, outdir, min_zoom, max_zoom)
+            tile(analysis, temptiff, outdir, min_zoom, max_zoom)
+
+            # delete the TIFF file
+            os.remove(temptiff)
+            print('Removed temporary TIFF file', temptiff)
 
         else:
             print('Error: Input file', infile, 'is not in GeoPDF format.')
 
+@click.command(name='pack')
+@click.option('--overwrite', default=False, is_flag=True, help='Overwrite existing MBTiles file')
+@click.option('--debug', default=False, is_flag=True, help='')
+@click.argument('TILESET')
+@click.argument('OUTFILE', default='__auto__')
+def mbtpack(overwrite: bool, debug: bool, tileset: str, outfile: str):
+    '''Pack a tileset into an MBTiles file'''
+
+    # get output filename if not specified
+    if outfile == '__auto__':
+        outfile = '%s.mbtiles' % tileset
+
+    # make sure things exist
+    if not os.path.isdir(tileset):
+        print('Error: Tileset', tileset, 'must be a tileset directory.')
+    elif os.path.isfile(outfile) and not overwrite:
+        print('Error: Output file', outfile, 'already exists.')
+
+    # all clear
+    else:
+        if os.path.isfile(outfile) and overwrite:
+            os.remove(outfile)
+            if debug:
+                print('Removed existing MBTiles file', outfile)
+        mbtiles_pack(tileset, outfile, debug=debug)
+
 @click.command()
 @click.option('--info', default=False, is_flag=True, help='Show file information in tab-delimited format')
+@click.option('--no-mbtiles', default=False, is_flag=True, help='Skip MBTiles file creation')
 @click.argument('FILES', nargs=-1)
-def batch(info: bool, files):
-    '''Batch conversion of GeoPDF files into tilesets'''
+def batch(info: bool, no_mbtiles: bool, files):
+    '''Batch conversion of GeoPDF files into MBTiles files; overwrites any files already existing.'''
 
     if len(files) > 0:
 
@@ -313,12 +478,53 @@ def batch(info: bool, files):
                     tuple(analysis['size']),
                     '%d-%d' % (analysis['minZoom'], analysis['maxZoom']),
                     sep='\t')
+
             elif analysis['format'] == 'PDF/Geospatial PDF':
                     infile = analysis['filename']
-                    outdir = os.path.splitext(infile)[0]
-                    temptiff = '%s.tiff' % outdir
+                    tiledir = os.path.splitext(infile)[0]
+                    temptiff = '%s_temp.tiff' % tiledir
+                    mbtiles = '%s.mbtiles' % tiledir
+
+                    if not no_mbtiles:
+                        tiledir = '%s_temp' % tiledir
+                        # remove the MBTiles file
+                        if os.path.exists(mbtiles):
+                            if os.path.isdir(mbtiles):
+                                shutil.rmtree(mbtiles)
+                            else:
+                                os.remove(mbtiles)
+
+                    # remove the TIFF file
+                    if os.path.exists(temptiff):
+                        if os.path.isdir(temptiff):
+                            shutil.rmtree(temptiff)
+                        else:
+                            os.remove(temptiff)
+
+                    # remove the tileset directory
+                    if os.path.exists(tiledir):
+                        if os.path.isdir(tiledir):
+                            shutil.rmtree(tiledir)
+                        else:
+                            os.remove(tiledir)
+
+                    # rasterize the PDF
                     rasterize(analysis, infile, temptiff)
-                    tile(analysis, temptiff, outdir)
+
+                    # tile the TIFF
+                    tile(analysis, temptiff, tiledir)
+
+                    # remove the TIFF
+                    os.remove(temptiff)
+                    print('Removed temporary TIFF file', temptiff)
+
+                    if not no_mbtiles:
+                        # pack the tiles into MBTiles
+                        mbtiles_pack(tiledir, mbtiles)
+
+                        # remove the tiles
+                        shutil.rmtree(tiledir)
+                        print('Removed temporary tileset directory', tiledir)
             else:
                 print('Warning: Skipping input file', analysis['filename'], 'not in GeoPDF format.')
 
@@ -330,5 +536,6 @@ if __name__ == '__main__':
     cli.add_command(info)
     cli.add_command(tiff)
     cli.add_command(tileset)
+    cli.add_command(mbtpack)
     cli.add_command(batch)
     cli()
