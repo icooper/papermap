24bafbb5001cd77f200d617a3494268ed1bd2a8e
Ian Cooper <me@iancooper.name>
2020-08-24 15:40:48 -0600

    added virtual power button for GSM/GNSS module

---
+++ gsm_power.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+GPIO.output(7, GPIO.LOW)
+time.sleep(4)
+GPIO.output(7, GPIO.HIGH)
+GPIO.cleanup()
